c7f1d36e7c645e8e168108e44dab947d3f8bd845
merlin2181 <merlin2181@gmail.com>
2020-08-13 20:17:33 -0700

    Stage 3 of 5 of the Tic Tac Toe with AI project from JetBrains Academy
    - refactored code into OOP
    - completely re-coded how the game checks for a winner, using an ordered dictionary, and was able to remove 3 methods
    - re-coded how the computer chooses a move. instead of guess and check with randint, the program now makes a list of empty cells and randomly chooses from the list using random.choice() to make a move
    - added new comments

--- Tic-Tac-Toe with AI/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe with AI/task/tictactoe/tictactoe.py
@@ -1,135 +1,174 @@
 # write your code here
-
-def print_cells(cell_list):
-    """
-    Outputs the current board to the screen
-    """
-    board = f"---------\n" \
-            f"| {cell_list[2][0]} {cell_list[2][1]} {cell_list[2][2]} |\n" \
-            f"| {cell_list[1][0]} {cell_list[1][1]} {cell_list[1][2]} |\n" \
-            f"| {cell_list[0][0]} {cell_list[0][1]} {cell_list[0][2]} |\n" \
-            f"---------"
-    print(board)
+from collections import OrderedDict
+import random
+import sys
 
 
-def check_coordinates(numbers, board):
-    """
-    Function that checks whether the coordinates entered by the user are "legal".
-    Returns an error message or True
-    """
-    if len(numbers) != 2:
-        return 'You should enter numbers!'
-    num_list = [1, 2, 3]
-    try:
-        numbers[0], numbers[1] = int(numbers[0]), int(numbers[1])
-    except ValueError:
-        return 'You should enter numbers!'
-    if numbers[0] not in num_list or numbers[1] not in num_list:
-        return 'Coordinates should be from 1 to 3!'
-    if board[numbers[1] - 1][numbers[0] - 1] != " ":
-        return 'This cell is occupied! Choose another one!'
-    return True
+class TicTacToe:
+
+    def __init__(self):
+        self.move = 0
+        self.player1 = None
+        self.player2 = None
+        self.board = OrderedDict({(i, j): " " for i in range(1, 4) for j in range(1, 4)})
+        self.winner = None
+        self.draw = None
+
+    def pick_player(self):
+        """
+        Method that starts the game by having the user choose to play against computer,
+        another user or have the computer play itself
+        """
+        player_choice = ['user', 'easy']
+        while True:
+            choice = input('Input Command: ').split()
+            if choice[0] == 'exit':
+                sys.exit()
+            elif choice[0] == 'start' and len(choice) == 3:
+                if choice[1] in player_choice and choice[2] in player_choice:
+                    self.player1 = [choice[1], 'X']
+                    self.player2 = [choice[2], 'O']
+                    self.print_gameboard()
+                    self.run_game()
+                else:
+                    print('Bad parameters!')
+            else:
+                print('Bad parameters!')
+
+    def run_game(self):
+        """
+        Method that runs the real code of the game. It has the player choose a position and
+        checks whether the game is won or a draw.
+        """
+        while True:
+            if self.move == 0:
+                self.choose_player(self.player1)
+                self.move += 1
+            else:
+                self.choose_player(self.player2)
+                self.move -= 1
+            self.print_gameboard()
+            self.check_winner()
+            if self.winner is None and self.draw is None:
+                continue
+            elif self.winner is not None:
+                print(f'{self.winner} wins')
+                self.board = OrderedDict({(i, j): " " for i in range(1, 4) for j in range(1, 4)})
+                self.winner = None
+                break
+            elif self.draw is not None:
+                print('Draw')
+                self.board = OrderedDict({(i, j): " " for i in range(1, 4) for j in range(1, 4)})
+                self.draw = None
+                break
+        self.pick_player()
+
+    def print_gameboard(self):
+        """
+        Outputs the current board to the screen
+        """
+        board_output = f"---------\n" \
+                       f"| {self.board[(3, 1)]} {self.board[(3, 2)]} {self.board[(3, 3)]} |\n" \
+                       f"| {self.board[(2, 1)]} {self.board[(2, 2)]} {self.board[(2, 3)]} |\n" \
+                       f"| {self.board[(1, 1)]} {self.board[(1, 2)]} {self.board[(1, 3)]} |\n" \
+                       f"---------"
+        print(board_output)
+
+    def choose_player(self, player):
+        """
+        Method that decides whether the computer is playing or the user
+        """
+        if player[0] == 'user':
+            return self.user_move(player[1])
+        elif player[0] == 'easy':
+            return self.computer_move(player[1])
+
+    def user_move(self, char):
+        """
+        method that lets the user choose a move
+        """
+        flag = True
+        while flag:
+            nums = input('Enter the coordinates: ').split()
+            check = self.check_coordinates(nums)
+            if type(check) == str:
+                print(check)
+            else:
+                flag = False
+        self.board[(nums[0], nums[1])] = char
+
+    def computer_move(self, char):
+        """
+        method that lets the computer choose a move by creating a list of empty cells and
+        randomly choosing a cell from that list
+        """
+        random.seed()
+        print('Making move level "easy"')
+        comp_move = []
+        for key, value in self.board.items():
+            if value == ' ':
+                comp_move.append(key)
+        self.board[random.choice(comp_move)] = char
+
+    def check_coordinates(self, numbers):
+        """
+        Method that checks whether the coordinates entered by the user are "legal".
+        Returns an error message or True
+        """
+        if len(numbers) != 2:
+            return 'You should enter numbers!'
+        num_list = [1, 2, 3]
+        try:
+            numbers[0], numbers[1] = int(numbers[0]), int(numbers[1])
+        except ValueError:
+            return 'You should enter numbers!'
+        if numbers[0] not in num_list or numbers[1] not in num_list:
+            return 'Coordinates should be from 1 to 3!'
+        if self.board[(numbers[0], numbers[1])] != " ":
+            return 'This cell is occupied! Choose another one!'
+        return True
+
+    def check_winner(self):
+        """
+        Method that determines if the game is finished or not and
+        whether X or O has won the game or it is a draw.
+        """
+        items = list(self.board.items())
+        moves = ['X', 'O']
+        for move in moves:
+            # Checks columns
+            for i in range(3):
+                if items[i][1] == move and items[i][1] == items[i + 3][1] == items[i + 6][1]:
+                    self.winner = items[i][1]
+            # Checks rows
+            for i in range(0, 8, 3):
+                if items[i][1] == move and items[i][1] == items[i + 1][1] == items[i + 2][1]:
+                    self.winner = items[i][1]
+            # Checks diagonals
+            if items[0][1] == move and items[0][1] == items[4][1] == items[8][1]:
+                self.winner = items[0][1]
+            elif items[2][1] == move and items[2][1] == items[4][1] == items[6][1]:
+                self.winner = items[2][1]
+        # If there is still no winner, it checks for draw
+        if self.winner is None:
+            self.check_draw()
+
+    def check_draw(self):
+        """
+        Totals all the Xs and Os on the board. If there are 9 total without a winner, then
+        this sets self.draw to True
+        """
+        exs = 0
+        ohs = 0
+        for value in self.board.values():
+            if value == 'X':
+                exs += 1
+            elif value == 'O':
+                ohs += 1
+        if exs + ohs == 9:
+            self.draw = True
 
 
-def check_winner(board):
-    """
-    Function that determines if the game is finished or not and
-    whether X or O has one the game.
-    """
-    col = check_columns(board)
-    row = check_rows(board)
-    diag = check_diagonal(board)
-    exes = ['X', 'X', 'X']
-    oohs = ['O', 'O', 'O']
-    winner = [col, row, diag]
-    for item in winner:
-        for i in range(len(item)):
-            if item[i] == exes or item[i] == oohs:
-                winning_move = item[i][0]
-                return winning_move
-    for item in winner:
-        for i in range(len(item)):
-            if ' ' in item[i]:
-                return "Game not finished"
-    return "Draw"
-
-
-def check_columns(board):
-    """
-    Gathers all the entries in each column going from left to right
-    """
-    check_col = [[], [], []]
-    for i in range(3):
-        for j in range(3):
-            check_col[i].append(board[j][i])
-    return check_col
-
-
-def check_rows(board):
-    """
-    Gathers all the entries in each row going from bottom to top
-    """
-    check_row = [[], [], []]
-    for i in range(3):
-        for j in range(3):
-            check_row[i].append(board[i][j])
-    return check_row
-
-
-def check_diagonal(board):
-    """
-    Gathers all the entries in each diagonal starting from bottom left to top right
-    and then from bottom right to top left
-    """
-    check_diag = [[], []]
-    for i in range(3):
-        check_diag[0].append(board[i][i])
-    j = 2
-    for i in range(3):
-        check_diag[1].append(board[i][j])
-        j -= 1
-    return check_diag
-
-
-starting_moves = input('Enter cells: ')
-starting_moves = [char if char != "_" else " " for char in starting_moves]
-
-# Determines if it's X's move or O's move
-if starting_moves.count('X') == starting_moves.count('O'):
-    move = 0
-else:
-    move = 1
-
-# Formats the input into the game board
-layout = [[], [], []]
-for i in range(3):
-    for _ in range(3):
-        layout[i].append(starting_moves.pop())
-for i in range(3):
-    layout[i].reverse()
-print_cells(layout)
-
-# Loop to asking the user for the correct coordinates
-flag = True
-while flag:
-    nums = input('Enter the coordinates: ').split()
-    check = check_coordinates(nums, layout)
-    if type(check) == str:
-        print(check)
-    else:
-        flag = False
-
-# Main part of the game
-nums = [(int(num) - 1) for num in nums]
-if move % 2 == 0:
-    layout[nums[1]][nums[0]] = 'X'
-    move += 1
-else:
-    layout[nums[1]][nums[0]] = 'O'
-    move -= 1
-print_cells(layout)
-outcome = check_winner(layout)
-if len(outcome) == 1:
-    print(f'{outcome} wins')
-else:
-    print(outcome)
+if __name__ == '__main__':
+    ttt = TicTacToe()
+    ttt.pick_player()
